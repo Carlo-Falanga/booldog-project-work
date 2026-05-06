10478a750449e62642cfab8357c727b3aac6624d
odon-airoldi <odon.airoldi@gmail.com>
2026-05-06 18:46:23 +0200

    add route and data fetch product page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DefaultLayout from "./layouts/DefaultLayout";
 import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Routes>
           <Route element={<DefaultLayout />}>
             <Route index element={<HomePage />} />
-            <Route />
+            <Route path="product/:slug" element={<ProductPage />} />
             <Route />
             <Route />
             <Route />
--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,6 +1,35 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
+
 export default function ProductPage() {
 
+    const [dataProduct, setDataProduct] = useState(null)
+
+    const { slug } = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/products/${slug}`)
+            .then(res => res.json())
+            .then(data => setDataProduct(data))
+    }, [slug])
+
+    console.log(dataProduct)
+
+
     return (
-        <h1>Product Page</h1>
+        <>
+            {dataProduct && (
+
+                <div>
+                    <h1>{dataProduct.name}</h1>
+                    <p>{dataProduct.description}</p>
+                    <p>{dataProduct.price}</p>
+                    <p>{dataProduct.stock}</p>
+                    <img src={dataProduct.image_url} alt={dataProduct.name} />
+                </div>
+            )
+            }
+        </>
     )
 }
