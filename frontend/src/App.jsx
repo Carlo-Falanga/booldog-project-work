f659778505ffb228b2880fca89a6caddb2e865a8
Carlo-Falanga <carlofalanga7@gmail.com>
2026-05-06 12:38:54 +0200

    add Bootstrap and React Router for layout and navigation; create header, footer, and home page components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DefaultLayout from "./layouts/DefaultLayout";
+import HomePage from "./pages/HomePage";
 
 function App() {
-
-
   return (
     <>
-
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route index element={<HomePage />} />
+            <Route />
+            <Route />
+            <Route />
+            <Route />
+            <Route />
+            <Route />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/AppFooter.jsx
@@ -0,0 +1,6 @@
+export default function AppFooter(){
+
+    return(
+        <h1>Footer</h1>
+    )
+}
