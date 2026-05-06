8dd458809dd90481505bed4ecd48e6a573a0afe5
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-05-06 13:27:06 +0200

    add base structure navbar

--- frontend/src/components/AppHeader.jsx
+++ frontend/src/components/AppHeader.jsx
@@ -1,6 +1,36 @@
-export default function AppHeader(){
+export default function AppHeader() {
 
-    return(
-        <h1>Header</h1>
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-white">
+                <div className="container">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                        </ul>
+                        <div id="nav_icons" className="d-flex gap-5 fs-4">
+                            <div>
+                                <i class="bi bi-heart"></i>
+                            </div>
+                            <div>
+                                <i class="bi bi-cart2"></i>
+                            </div>
+                            <div>
+                                <i class="bi bi-search"></i>
+                            </div>
+                        </div>
+
+
+
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
