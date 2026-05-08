84d5f2747d4209acc5b42847601dbc7c0ca7ff9e
pietrobertino <pietrobertino20@gmail.com>
2026-05-08 13:26:41 +0200

    style index page and cards, add missing image

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -17,18 +17,30 @@ export default function SearchPage() {
 
     return (
         <>
-            <h1 className='text-center mt-3'>Ricerca prodotti</h1>
+            <h1 className='text-center my-3'>Ricerca prodotti</h1>
             <div className="container">
-                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3">
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
                     {productList.map(product => (
                         <div className="col" key={product.id}>
-                            <div className="card">
-                                {product.name}
-                                {product.price}
-                                {product.category}
-                                {product.img_url}
-                                {product.brand_name}
-                                {product.animal_name}
+                            <div className="card p-3 h-100">
+                                <div className="card-img card-header p-3">
+                                    <img src={`http://localhost:3000/images/products/${product.img_url}`} alt={`${product.name}'s picture`} />
+                                </div>
+                                <div className=' d-flex align-items-center justify-content-between opacity-75 px-3 pt-3' >
+                                    <div>
+                                        {product.category}
+                                    </div>
+                                    <div>
+                                        {product.animal_name}
+                                    </div>
+                                </div>
+                                <div className='card-body'>
+                                    <h3 className='card-title'>
+                                        {product.name}
+                                    </h3>
+                                    <div className=' fs-4'> &euro; {product.price}</div>
+                                </div>
+
                             </div>
                         </div>
                     ))}
@@ -43,4 +55,8 @@ export default function SearchPage() {
 
 
 // Missione attuale: 
-// abbellire la pagina e le card, inserendoci tutte le informazioni del caso
+// implementare la barra di ricerca
+
+// Missioni future:
+// implementeare le diverse opzioni di ordinamento
+// fittare il design della pagina con quello del progetto
