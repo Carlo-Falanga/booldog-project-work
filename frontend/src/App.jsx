207ad9ce6a5f06e0c56ecac9fbafd9c5d2b4e074
odon-airoldi <odon.airoldi@gmail.com>
2026-05-08 17:11:01 +0200

    refactor integrate global cart state into ProductPage component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,30 @@ import DefaultLayout from "./layouts/DefaultLayout";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
+import { useState, useEffect } from "react";
+
 
 function App() {
+
+  const [globalCart, setGlobalCart] = useState(
+    JSON.parse(localStorage.getItem("globalCart")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("globalCart", JSON.stringify(globalCart));
+  }, [globalCart]);
+
+
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />}>
             <Route index element={<HomePage />} />
-            <Route path="product/:slug" element={<ProductPage />} />
+            <Route path="product/:slug" element={<ProductPage globalCart={globalCart} setGlobalCart={setGlobalCart} />} />
             <Route />
             <Route />
-            <Route path="/cart" element={<CartPage/>} />
+            <Route path="/cart" element={<CartPage />} />
             <Route />
             <Route />
             <Route />
