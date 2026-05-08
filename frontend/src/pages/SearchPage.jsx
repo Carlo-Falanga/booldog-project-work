75eb17f4101068a2a40f3e033cdbf17068b4aa9c
pietrobertino <pietrobertino20@gmail.com>
2026-05-08 12:06:03 +0200

    craete and link search page

---
+++ frontend/src/pages/SearchPage.jsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default function SearchPage() {
+
+
+    return (
+        <h1>Search Page</h1>
+    )
+}
+
+
+
+
+
+
+
+
+
+// Missione attuale: 
+// fare la chiamata api index per mostrare tutti i prodotti
