a91b8e759aea500be48c6efbf7efcc558f066763
nabil-hajali <nabil2491@gmail.com>
2026-05-10 21:09:35 +0200

    refactor: redesign AppHeader and AppFooter, implement HomePage with product fetching and filtering

--- frontend/src/components/AppHeader.jsx
+++ frontend/src/components/AppHeader.jsx
@@ -1,39 +1,54 @@
 import { NavLink } from "react-router-dom"
 import CartPage from "../pages/CartPage"
+import axios from "axios"
 
 export default function AppHeader() {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg bg-white">
-                <div className="container">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                        </ul>
-                        <div id="nav_icons" className="d-flex justify-content-around gap-5 fs-4">
-                            <div>
-                                <i class="bi bi-heart"></i>
-                            </div>
-                            <NavLink to='/cart'>
-                                <i class="bi bi-cart2"></i>
-                            </NavLink>
-                            <div>
-                                <i class="bi bi-search"></i>
-                            </div>
-                        </div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+        <a className="navbar-brand fw-bold fs-4" href="#">
+          🐾 BoolDog
+        </a>
 
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" href="#">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Cani</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Gatti</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Offerte</a>
+            </li>
+          </ul>
 
-                    </div>
-                </div>
-            </nav>
+          <div className="d-flex gap-2 align-items-center">
+            <span className="text-white position-relative" style={{ cursor: 'pointer', fontSize: '1.3rem' }}>
+              🛒
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style={{ fontSize: '0.6rem' }}>
+                0
+              </span>
+            </span>
+            <button className="btn btn-outline-light btn-sm">Accedi</button>
+            <button className="btn btn-warning btn-sm fw-semibold">Registrati</button>
+          </div>
+        </div>
+      </nav>
+
         </>
     )
 }
