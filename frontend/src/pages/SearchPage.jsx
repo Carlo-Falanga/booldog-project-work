a6ebb37d95303b3b3bd5654e368d1943ce9ea947
pietrobertino <pietrobertino20@gmail.com>
2026-05-11 12:31:14 +0200

    add order feature

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
 
 export default function SearchPage() {
 
-    const [initialProducts, setInitialProducts] = useState([]);
-
+    const [products, setProducts] = useState([]);
     const [search, setSearch] = useState("");
+    const [order, setOrder] = useState("");
 
     const url = `http://localhost:3000/products`
 
@@ -13,9 +13,34 @@ export default function SearchPage() {
     useEffect(() => {
         axios.get(url)
             .then(res => {
-                setInitialProducts(res.data);
+                setProducts(res.data);
             })
     }, [])
+
+    //calcolo l'array di prodotti finali, filtrando per ricerca ed ordinando i risultati
+    const finalProducts = useMemo(() => {
+        return products
+            .filter(product => product.name.toLowerCase().includes(search.toLowerCase()))
+            .toSorted((a, b) => {
+                switch (order) {
+                    case "price-up":
+                        return a.price - b.price;
+                        break;
+                    case "price-down":
+                        return b.price - a.price;
+                        break;
+                    case "name":
+                        return a.name.localeCompare(b.name);
+                        break;
+                    case "recent":
+                        return new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime();
+                        break;
+                    default:
+                        return 0;
+                }
+            })
+
+    }, [products, search, order])
 
 
 
@@ -27,20 +52,22 @@ export default function SearchPage() {
             <h1 className='text-center my-3'>Ricerca prodotti</h1>
             <div className='container d-flex align-items-center justify-content-between mb-3'>
                 {/* searchbar */}
-                <input type="text" className='form-control d-inline w-25' placeholder='Ricerca un prodotto...  ' value={search} onChange={(e) => handleSearch(e.target.value)} />
+                <input type="text" className='form-control d-inline w-25' placeholder='Ricerca un prodotto...  ' value={search} onChange={(e) => setSearch(e.target.value)} />
                 {/* filters */}
-                <select name="product-order" id="" className='form-select w-25' onChange={(e) => handleSelect(e.target.value)}>
-                    <option defaultValue="">Ordina i prodotti per</option>
-                    <option value="price-up">Prezzo crescente</option>
-                    <option value="price-down">Prezzo decrescente</option>
-                    <option value="name">Nome</option>
-                    <option value="recent">Recenti</option>
-                </select>
+                <div className='d-flex gap-3 align-items-baseline'>
+                    <label htmlFor="product-order" className=' form-label flex-shrink-0'>Ordina i prodotti per</label>
+                    <select name="product-order" id="product-order" className='form-select w-100' onChange={(e) => setOrder(e.target.value)}>
+                        <option value="recent">Recenti</option>
+                        <option value="name">Nome</option>
+                        <option value="price-up">Prezzo crescente</option>
+                        <option value="price-down">Prezzo decrescente</option>
+                    </select>
+                </div>
             </div>
             <div className="container">
-                {initialProducts.length > 0 ?
-                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
-                        {initialProducts.map(product => (
+                {finalProducts.length > 0 ?
+                    <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 mb-3">
+                        {finalProducts.map(product => (
                             <div className="col" key={product.id}>
                                 <div className="card p-3 h-100">
                                     <div className="card-img card-header p-3">
