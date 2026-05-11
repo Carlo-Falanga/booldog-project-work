c0a29142fbe7e461d7db1974bb75d8b3d7a5d543
odon-airoldi <odon.airoldi@gmail.com>
2026-05-11 19:32:50 +0200

    feat: add WishListPage component and integrate into routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,38 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { CartContextProvider } from "./context/CartContext";
 import DefaultLayout from "./layouts/DefaultLayout";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
-import { useState, useEffect } from "react";
-import { CartContextProvider } from "./context/CartContext";
-
 import SearchPage from "./pages/SearchPage";
+import WishListPage from "./pages/WishListPage";
+
 
 function App() {
 
   return (
     <>
-     <CartContextProvider>
+      <CartContextProvider>
 
-      <BrowserRouter>
-        <Routes>
-          <Route element={<DefaultLayout />}>
-            <Route index element={<HomePage />} />
-            <Route path="product/:slug" element={<ProductPage />} />
-            <Route path="/search" element={<SearchPage />} />
-            <Route path="/checkout" element={<CheckoutPage />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route />
-            <Route />
-            <Route />
-            <Route />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<DefaultLayout />}>
+              <Route index element={<HomePage />} />
+              <Route path="product/:slug" element={<ProductPage />} />
+              <Route path="/search" element={<SearchPage />} />
+              <Route path="/checkout" element={<CheckoutPage />} />
+              <Route path="/cart" element={<CartPage />} />
+              <Route path="/wishlist" element={<WishListPage />} />
+              <Route />
+              <Route />
+              <Route />
+            </Route>
+          </Routes>
+        </BrowserRouter>
 
-     </CartContextProvider>
+      </CartContextProvider>
     </>
   );
 }
