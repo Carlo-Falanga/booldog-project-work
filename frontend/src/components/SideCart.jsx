aa4c21fccc47767b44605838e95253572f4674fc
Carlo-Falanga <carlofalanga7@gmail.com>
2026-05-11 13:16:53 +0200

    feat: refactor SideCart component to include CartTotal and improve structure

--- frontend/src/components/SideCart.jsx
+++ frontend/src/components/SideCart.jsx
@@ -1,20 +1,30 @@
 import { useGlobal } from "../context/CartContext";
 import { Link } from "react-router-dom";
 import CartProductsList from "./CartProductsList";
+import CartTotal from "./CartTotal";
 
-export default function SideCart() {
-
-
+export default function SideCart({setAsideCart}) {
   return (
     <div className="position-fixed top-0 end-0 h-100 bg-white shadow-lg p-3 w-50">
       <div className="mb-5">
         <h3 className="h5">Aggiunto al carrello</h3>
-        <CartProductsList/>
-        <Link to="/cart" className="btn btn-primary">
-          Vai al carrello
-        </Link>
+        <CartProductsList />
+
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => setAsideCart(false)}
+          >
+            Chiudi
+          </button>
+
+          <Link to="/cart" className="btn btn-primary">
+            Vai al carrello
+          </Link>
+        </div>
+
+        <CartTotal />
       </div>
-      <button onClick={() => setAsideCart(false)}>Chiudi</button>
     </div>
   );
 }
