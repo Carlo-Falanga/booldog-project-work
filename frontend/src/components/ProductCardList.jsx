c4af709397fe048bfd3e0027b076dbc7fbdfbbc7
odon-airoldi <odon.airoldi@gmail.com>
2026-05-12 13:08:37 +0200

    feat: add ProductCardList component and enhance wishlist display options in WishListPage

---
+++ frontend/src/components/ProductCardList.jsx
@@ -0,0 +1,46 @@
+import { useWishlist } from "../context/WishListContext";
+import { Link, useParams } from "react-router-dom";
+
+export default function ProductCardList({ product }) {
+  const { wishlist, setWishlist } = useWishlist();
+
+  // verifico se il prodotto esiste nel carrello
+  const existingProductWL = wishlist.find((item) => item.slug === product.slug);
+
+  // funzione aggiungi wishlist
+  const addToWishList = () => {
+    // se esiste al click lo rimuovo
+    if (existingProductWL) {
+      const updatedWishList = wishlist.filter((item) => item.slug !== product.slug);
+      setWishlist(updatedWishList);
+    } else {
+      // altrimenti lo aggiungo
+      setWishlist([...wishlist, { ...product }]);
+    }
+  };
+
+  return (
+
+    <div className="card p-3 h-100 position-relative">
+      <button onClick={addToWishList} className="btn position-absolute start-0 pe-4 z-1 border-0">
+        <i className={`bi ${existingProductWL ? "bi-heart-fill" : "bi-heart"}`}></i>
+      </button>
+      <Link to={`/product/${product.slug}`} className='text-decoration-none text-reset row'>
+        <div className="col-2 p-3">
+          <img className="img-fluid"
+            src={`http://localhost:3000/images/products/${product.img_url}`}
+            alt={`${product.name}'s picture`}
+          />
+        </div>
+        <div className="col-5 opacity-75">
+          <div>{product.category}</div>
+          <div>{product.animal_name}</div>
+        </div>
+        <div className="col-5">
+          <h3 className="card-title">{product.name}</h3>
+          <div className=" fs-4">&euro; {product.price}</div>
+        </div>
+      </Link>
+    </div >
+  );
+}
