de82e0623d8d35b6dbfb4eec2b7def4dcc7db4c1
Antonio Falletta <antoniofalletta2000@gmail.com>
2026-05-14 00:06:43 +0200

    enhance OrderConfirmedPage layout with improved styling for order details

--- frontend/src/pages/OrderConfirmedPage.jsx
+++ frontend/src/pages/OrderConfirmedPage.jsx
@@ -22,30 +22,36 @@ export default function OrderConfirmedPage() {
         <>
             <div className="container mt-5">
 
-                <div className="d-flex justify-content-start mb-3">
-                    <Link to="/">
-                        <button className="btn btn-dark rounded-pill border-0 btn_cart"><i class="bi bi-arrow-left-short"></i>Torna alla Home</button>
-                    </Link>
-                </div>
-
                 <div className="card rounded-4 p-4">
                     <h1 className="d-flex justify-content-center">Ordine confermato!</h1>
                     <span className="fw-medium d-flex justify-content-center m-3">Dati di fatturazione</span>
                     <ul className="list-unstyled">
-                        <li>Cliente: {data.user_full_name}</li>
-                        <li>Email : {data.email}</li>
-                        <li>Telefono : {data.phone_number}</li>
-                        <li>Paese : {data.country}</li>
-                        <li>Indirizzo : {data.address}, {data.zipcode}, {data.city}</li>
+                        <li><span className="fw-semibold">Cliente:</span> {data.user_full_name}</li>
+                        <li><span className="fw-semibold">Email:</span> {data.email}</li>
+                        <li><span className="fw-semibold">Telefono:</span> {data.phone_number}</li>
+                        <li><span className="fw-semibold">Paese:</span> {data.country}</li>
+                        <li><span className="fw-semibold">Indirizzo:</span> {data.address}, {data.zipcode}, {data.city}</li>
                     </ul>
                     <hr />
                     <span className="fw-medium d-flex justify-content-center m-3">Dati dell'ordine</span>
                     <ul className="list-unstyled">
-                        <li>Codice Ordine: {data.order_code}</li>
-                        <li>Data : {data.created_at}</li>
-                        <li>Totale : {data.total}€</li>
-                        <li>Stato : {data.status}</li>
+                        <li><span className="fw-semibold">Codice Ordine:</span> {data.order_code}</li>
+                        <li><span className="fw-semibold">Data:</span> {new Date(data.created_at).toLocaleDateString("it-IT", {
+                            day: "2-digit",
+                            month: "long",
+                            year: "numeric",
+                            hour: "2-digit",
+                            minute: "2-digit"
+                        })}</li>
+                        <li><span className="fw-semibold">Totale:</span> {data.total}€</li>
+                        <li><span className="fw-semibold">Stato:</span> {data.status}</li>
                     </ul>
+                    <hr />
+                    <div className="d-flex justify-content-center align-items-center m-3">
+                        <Link to="/">
+                            <button className="btn btn-dark rounded-pill border-0 btn_cart"><i class="bi bi-arrow-left-short"></i>Torna alla Home</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </>
