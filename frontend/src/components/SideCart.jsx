dbbcfecd8cd4a375cb80e0beb70261205af73a80
odon-airoldi <odon.airoldi@gmail.com>
2026-05-15 18:29:14 +0200

    refactor: enhance SideCart component structure and styling, add overlay and transition effects

--- frontend/src/components/SideCart.jsx
+++ frontend/src/components/SideCart.jsx
@@ -11,35 +11,35 @@ export default function SideCart() {
     addToCart
   } = useGlobal();
 
+  console.log(asideCart)
+
   return (
     <>
-      {asideCart && (
-        <div>
-          <div onClick={() => setAsideCart(false)} className="bg-body-secondary bg-opacity-50 position-fixed start-0 end-0 top-0 bottom-0 z-2">
-          </div>
-          <aside className="aside-cart position-fixed top-0 end-0 h-100 bg-white shadow-lg p-3 z-3 sidebar_width overflow-scroll">
-            <div className="mb-5">
+      {/* {asideCart && ()} */}
+      <div>
+        <div onClick={() => setAsideCart(false)} className="overlay bg-body-secondary position-fixed start-0 end-0 top-0 bottom-0 z-2">
+        </div>
+        <aside className="aside-cart position-fixed top-0 h-100 bg-white shadow-lg p-3 z-3 sidebar_width overflow-scroll">
+          <div className="mb-5">
 
-              <button
-                className="btn-close d-flex ms-auto mb-3"
-                aria-label="Close"
-                onClick={() => setAsideCart(false)}
-              ></button>
+            <button
+              className="btn-close d-flex ms-auto mb-3"
+              aria-label="Close"
+              onClick={() => setAsideCart(false)}
+            ></button>
 
-              <CartProductsList />
+            <CartProductsList />
 
-              <CartTotal />
+            <CartTotal />
 
-              <div className="d-flex justify-content-end text-decoration-none">
-                <Link to="/cart" className="btn btn-dark btn-sm rounded-pill w-50 py-3 mt-4 border-0 btn_cart">
-                  Vai al carrello
-                </Link>
-              </div>
+            <div className="d-flex justify-content-end text-decoration-none">
+              <Link to="/cart" className="btn btn-dark btn-sm rounded-pill w-50 py-3 mt-4 border-0 btn_cart">
+                Vai al carrello
+              </Link>
             </div>
-          </aside>
-        </div >
-      )
-      }
+          </div>
+        </aside>
+      </div>
     </>
   );
 }
